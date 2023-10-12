f21ee26184dcd2c30489cc058d287608f0483f74
Ely Frank <ely.frank@websis.com.br>
2023-10-12 12:27:15 -0300

    corrigindo build

--- docker/run.sh
+++ docker/run.sh
@@ -2,17 +2,18 @@
 
 if [ ! -f .env ]; then
     cp .env.example .env
+    php artisan key:generate
 fi
 
 cp ./docker/apache/000-default.conf /etc/apache2/sites-available
+
+chmod -R 777 storage
 
 if [ ! -f /usr/local/bin/composer ]; then
    curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 fi
 
 composer install
-
-php artisan key:generate
 
 a2enmod rewrite
 apache2-foreground
